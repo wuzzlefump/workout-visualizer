b295bda683e16884cee993edb817f5f81189c45c
Paul Elliott <paulelliott@pop-os.localdomain>
2023-01-08 13:06:31 -0700

    loading change

--- components/Loading.js
+++ components/Loading.js
@@ -1,17 +1,16 @@
-import {Circle} from "better-react-spinkit"
+import { Circle } from "better-react-spinkit";
 
-import React from 'react'
+import React from "react";
 
 function Loading() {
-    return (
-        <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-50">
-            <div className={" mt-52"}>
-                <Circle color={"black"} size={60}/>
-                <p className="font-semibold text-lg">Loading...</p>
-            </div>
-            
-        </div>
-    )
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-800  ">
+      <div className={" mt-52"}>
+        <Circle color={"white"} size={60} />
+        <p className="font-semibold text-lg text-gray-200">Loading...</p>
+      </div>
+    </div>
+  );
 }
 
-export default Loading
+export default Loading;
